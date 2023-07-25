3c4b6e910954e3f3384a6f2649b2df5e3fe8d0a8
Lucas Ceratto <lucasceratto@gmail.com>
2023-07-25 18:25:30 -0300

    Añado gran parte del layout
    Componentes:
    - LoggedLayout
    - Footer
    - LeftMenu
    - TopBar
    - Player
    
    Logica:
    - user

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Image, Input, Icon } from "semantic-ui-react"
+import { Player } from "../../Shared"
+import "./Footer.scss"
+
+export function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer__left'>
+        <Image src={null} />
+        <span>SONG NAME</span>
+      </div>
+
+      <div className='footer__center'>
+        <Player />
+      </div>
+
+      <div className='footer__right'>
+        <Input
+          type="range"
+          min={0}
+          max={1}
+          step={0.01}
+          label={<Icon name="volume up" />}
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Layout/TopBar/TopBar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Icon, Image } from "semantic-ui-react"
+import { Link, useNavigate } from "react-router-dom"
+import { Auth, User } from "../../../api"
+import { defaultUser } from "../../../assets"
+import "./TopBar.scss"
+
+const auth = new Auth()
+const user = new User()
+
+export function TopBar() {
+  //variables
+  const navigation = useNavigate()
+  const userData = user.getMe()
+  const displayName = userData.displayName || "Mi cuenta"
+  const avatar = userData.photoURL || defaultUser
+
+  //functions
+  const goBack = () => {
+    navigation(-1)
+  }
+
+  const logout = () => {
+    auth.logout()
+  }
+
+  //render
+  return (
+    <div className='top-bar'>
+      <Icon
+        name="angle left"
+        className="top-bar__back"
+        link
+        onClick={goBack}
+      />
+
+      <div className='top-bar__right'>
+        <Link to="/profile">
+          <Image src={avatar} avatar />
+          <span>{displayName}</span>
+        </Link>
+        <Icon
+          name="power"
+          link
+          onClick={logout}
+        />
+      </div>
+    </div>
+  )
+}
